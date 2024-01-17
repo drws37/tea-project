d883de91a23a7453c8629b5b2d233b65ab41b467
maksimkrylov <150614120+maksimkrylov@users.noreply.github.com>
2024-01-17 15:51:24 +0300

    server2.0

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,19 +1,21 @@
-const React = require('react');
-const NavBar = require('./NavBar');
+const React = require("react");
+const NavBar = require("./NavBar");
 
 module.exports = function Layout({ title, children, user }) {
   return (
     <html lang="en">
       <head>
         <title>{title}</title>
-        <link href="/styles/bootstrap.css" rel="stylesheet" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="/styles/bootstrap.css" />
-        {/* <script defer src="/scripts/auth.js" /> */}
+        <link
+          href="/styles/bootstrap.css"
+          rel="stylesheet"
+          crossOrigin="anonymous"
+        />
+        <link rel="stylesheet" href="/css/bootstrap.css" />
+        <link rel="stylesheet" href="/css/styles.css" />
+        <script defer src="/js/map.js" />
       </head>
-      <body>
-        <NavBar user={user} />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 };
