f2eda690f7850aca62fa65e40af33789cb300ef4
Ivan Kabatskii <ivankabatsky@gmail.com>
2024-01-17 12:23:10 +0300

    welcome to the club

---
+++ components/Layout.jsx
@@ -0,0 +1,19 @@
+const React = require('react');
+const NavBar = require('./NavBar');
+
+module.exports = function Layout({ title, children, user }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>{title}</title>
+        <link href="/styles/bootstrap.css" rel="stylesheet" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="/styles/style.css" />
+        {/* <script defer src="/scripts/auth.js" /> */}
+      </head>
+      <body>
+        <NavBar user={user} />
+        {children}
+      </body>
+    </html>
+  );
+};
