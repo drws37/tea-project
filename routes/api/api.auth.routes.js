0518809211d4437cb19517b28882588e0c4235e6
Ivan Kabatskii <ivankabatsky@gmail.com>
2024-01-19 10:40:12 +0300

    123

--- routes/api/api.auth.routes.js
+++ routes/api/api.auth.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const multer = require('multer');
 const { User } = require('../../db/models');
-const generateTokens = require('../../Utils/authUtils');
+const generateTokens = require('../../utils/authUtils');
 const configJWT = require('../../middleware/configJWT');
 
 const storage = multer.diskStorage({
