d4646156971116cd92c88e0ad2da2c9427081405
Gonamen <sashakim123321@gmail.com>
2024-01-19 15:47:19 +0300

    finish2

--- components/LoginPage.jsx
+++ components/LoginPage.jsx
@@ -4,23 +4,26 @@ const Layout = require('./Layout');
 function LoginPage({ title }) {
   return (
     <Layout title={title}>
-      <form id="log-page">
-        <div className="mb-3">
-          <h1>Авторизация</h1>
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Login
+      <div className="logapage">
+        <form id="log-page" className="loga">
+          <div className="mb-3">
+            <h1>Авторизация</h1>
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Login
+            </label>
             <input name="name" type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          </label>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
             <input name="password" type="password" className="form-control" id="exampleInputPassword1" />
-          </label>
-        </div>
-        <div className="mb-3 form-check" />
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+          </div>
+          <div className="mb-3 form-check" />
+          <button type="submit" className="btn btn-primary btn-dark">Submit</button>
+        </form>
+      </div>
+
     </Layout>
 
   );
